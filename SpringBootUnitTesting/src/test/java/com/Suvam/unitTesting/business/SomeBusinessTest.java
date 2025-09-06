f3abfb8378f875ff5d13d3e84921365556316fc6
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-06 20:49:00 +0530

    Unit test for a simple business service (summation of all element of array)

---
+++ SpringBootUnitTesting/src/test/java/com/Suvam/unitTesting/business/SomeBusinessTest.java
@@ -0,0 +1,36 @@
+package com.Suvam.unitTesting.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SomeBusinessTest {
+
+	@Test
+	public void calculateSum_basic() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] {1,2,3});
+		int expectedResult = 6;
+		assertEquals(expectedResult,actualResult);
+		
+	}
+	
+	@Test
+	public void calculateSum_empty() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] {});
+		int expectedResult = 0;
+		assertEquals(expectedResult,actualResult);
+		
+	}
+	
+	@Test
+	public void calculateSum_oneValue() {
+		SomeBusinessImpl business = new SomeBusinessImpl();
+		int actualResult = business.calculateSum(new int[] {10});
+		int expectedResult = 10;
+		assertEquals(expectedResult,actualResult);
+		
+	}
+
+}
