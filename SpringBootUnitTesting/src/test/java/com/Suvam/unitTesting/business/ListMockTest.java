4afbe1025ffb48ebc0182b5da91b9fc8e980f212
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-07 09:17:43 +0530

    Multiple return values and Specific Argument matchers

---
+++ SpringBootUnitTesting/src/test/java/com/Suvam/unitTesting/business/ListMockTest.java
@@ -0,0 +1,34 @@
+package com.Suvam.unitTesting.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+class ListMockTest {
+
+	List mock = mock(List.class);
+	
+	@Test
+	public void size_basic() {		
+		when(mock.size()).thenReturn(5);
+		assertEquals(5, mock.size());
+	}
+	
+	@Test
+	public void returnDifferentValues() {
+		when(mock.size()).thenReturn(5).thenReturn(10);
+		assertEquals(5, mock.size());
+		assertEquals(10, mock.size());
+	}
+	
+	@Test
+	public void returnWithParameters() {
+		when(mock.get(0)).thenReturn("suvam");
+		assertEquals("suvam", mock.get(0));
+	}
+
+}
