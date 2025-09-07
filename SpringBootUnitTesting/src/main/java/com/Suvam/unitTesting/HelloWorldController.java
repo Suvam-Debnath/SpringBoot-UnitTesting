cb973331cb15c88a0559be8c62e3d8861b86dd46
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-07 20:16:02 +0530

    Creating a Hello World Controller

---
+++ SpringBootUnitTesting/src/main/java/com/Suvam/unitTesting/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.Suvam.unitTesting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+}
