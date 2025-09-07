fe0d06c74d6a9e362a35c14751d7d95180a3d979
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-07 20:58:51 +0530

    Using Mock Mvc to test Hello World Controller

--- SpringBootUnitTesting/src/main/java/com/Suvam/unitTesting/controller/HelloWorldController.java
+++ SpringBootUnitTesting/src/main/java/com/Suvam/unitTesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.Suvam.unitTesting;
+package com.Suvam.unitTesting.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
