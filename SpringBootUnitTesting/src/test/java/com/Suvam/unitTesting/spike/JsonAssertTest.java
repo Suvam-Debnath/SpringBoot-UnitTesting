1bc3c449ac5273c60f4a7f288ae6552cf1d1a536
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-10 08:31:13 +0530

    JSON assert updated

--- SpringBootUnitTesting/src/test/java/com/Suvam/unitTesting/spike/JsonAssertTest.java
+++ SpringBootUnitTesting/src/test/java/com/Suvam/unitTesting/spike/JsonAssertTest.java
@@ -25,7 +25,7 @@ public class JsonAssertTest {
 	@Test
 	public void jsonAssert_WithoutEscapeCharacters() throws JSONException {
 		String expectedResponse = "{id:1,name:Ball,price:10}";
-		//jsonAssert StrictFalse should not exact match except for spaces
+		
 		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
 	}
 }
