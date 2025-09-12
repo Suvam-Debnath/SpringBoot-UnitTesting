b541821b3d1fd634d21ee9f7368d687282e731d2
Suvam Debnath <debnathsuvam322@gmail.com>
2025-09-12 07:57:19 +0530

    Create item entity and populate data with data.sql

--- SpringBootUnitTesting/src/main/java/com/Suvam/unitTesting/model/Item.java
+++ SpringBootUnitTesting/src/main/java/com/Suvam/unitTesting/model/Item.java
@@ -1,12 +1,19 @@
 package com.Suvam.unitTesting.model;
 
-public class Item {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 
+@Entity
+public class Item {
+	@Id
 	private int id;
 	private String name;
 	private int price;
 	private int quantity;
-
+	
+	@Transient
+	private int value;
 	
 	public int getId() {
 		return id;
@@ -22,6 +29,14 @@ public class Item {
 
 	public int getQuantity() {
 		return quantity;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
 	}
 
 	public Item(int id, String name, int price, int quantity) {
